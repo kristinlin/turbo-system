161cc3f2f76404a5412f419fb40fe478a38548d4
sabrina-wen <sabrinawn253@gmail.com>
2018-01-09 00:08:36 -0500

    added structs

---
+++ board.h
@@ -0,0 +1,27 @@
+// structs
+
+#include <stdio.h>
+#include <string.h>
+
+struct Space { // where everyone and their properties are on the board
+  char * name;
+  int chance; // 0 for don't draw or 1 for draw
+  int properties[4];
+};
+
+struct Chance { // for chance deck
+  char * text;
+  int money;
+  int space;
+};
+
+struct Start { // idk
+  int index;
+  int gains;
+};
+
+struct Turn {
+  int dead; // bankrupt or nah
+  int curr_index; // where you are right window
+  int dues[4]; //how much money you owe to each player
+};
--- makefile
+++ makefile
@@ -8,6 +8,7 @@ sdl: main.c
 	gcc -I/usr/include/SDL/ main.c -o sdltest -L/usr/lib -lSDL2
 runsdl: sdl
 	./sdltest
+board: board.h
+	gcc -o board board.h
 clean:
 	rm *~; rm *#; rm monopoly; rm *.o;
-
