e5113781460146b364e957882e3d413fcaa53634
bleung329 <bleung@stuy.edu>
2018-01-09 15:35:37 -0500

    init stuff

--- client.c
+++ client.c
@@ -1,13 +1,38 @@
 #include "pipe_networking.h"
 #include "pipe_networking.c"
 
+int money;
+typedef struct space{
+  char * name;
+  int chance;
+  int properties[4];  
+};
+
+typedef struct chance{
+  char * text;
+  int money;
+  int space;
+};
+
+typedef struct start{
+  int index;
+  int gains;
+};
+
+typedef struct turn{
+  int dead;
+  int curr_index;
+  int dues [4];
+};
+
 int main() {
 
   int to_server;
   int from_subserver;
   char buffer[BUFFER_SIZE];
+  struct chance Space;
 
-  from_subserver = client_handshake( &to_server );
+  //from_subserver = client_handshake( &to_server );
 
   /*  while (1) {
     printf("enter data: ");
@@ -18,4 +43,5 @@ int main() {
     printf("received: [%s]\n", buffer);
   }
   */
+  printf("%d bytes large\n",sizeof() );
 }
