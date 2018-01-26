a1b599778e550f70bda019781890adb13b1fedc7
sabrina-wen <sabrinawn253@gmail.com>
2018-01-25 23:48:22 -0500

    added monopoly gameplay or something

--- client.c
+++ client.c
@@ -29,7 +29,7 @@ int main() {
   printf("====================================================\n");
   printf("WELCOME TO MONOPOLY.\n");
   printf("You are officially player [%d]\n", player_num);
-	
+
 
   if (buff != buff_rec) {
     printf("Error. Not connected.\n");
@@ -48,40 +48,192 @@ int main() {
     if (new_update->curr_player == player_num) {
 
       printf("\n\nIT IS YOUR TURN. MAKE YOUR MOVE NOW.\n");
-    
+
       // // rand int 1 - 12
 
       int dice = rand() % 12 + 1;
       printf("YOU JUST ROLLED THE DICE: %d\n", dice);
       // // sets turn's curr index correctly
-      new_turn->curr_index = new_update->position[player_num] + dice;
+      new_update->position[player_num] += dice;
+      new_turn->curr_index = new_update->position[player_num];
       if (new_turn->curr_index > 39) {
-	money += 200;
-	new_turn->curr_index = new_turn->curr_index % 40;
+	       money += 200;
+         new_update->gains[player_num] = money;
+	       new_turn->curr_index = new_turn->curr_index % 40;
+         new_update->position[player_num] = new_turn->curr_index;
       }
 
       // get struct space in shm and manage sem
       gate(SPACES, ENTER);
       struct spaces * curr_space = getshm_space(new_turn->curr_index);
+
       gate(SPACES, LEAVE);
       printf("YOU ARE ON SPACE: %s\n", curr_space->name);
 
-      // // check if chance card; gate(CHANCE, ENTER AND LEAVE) before and after
-      // // // look at contents
-      // // // free the mem from chance_card
-      // // check if rent
-      // // // update client money; etc. 
-      // // ask user if they want property (if available)
-      // // // change money
-      // // // change struct space
-      // // write turn back
-      // // free the mem for curr_space
-      // // if dead, exit now
-    
+      // curr_space = spaces struct
+      // new_turn = turn struct
+      // new_update = update struct
+      //check to see card type
+      // check to see if chance card
+      if (curr_space->chance == 1) {
+        // add money to bank if there is money
+        if (curr_space->change_money > 0) {
+          printf("YOU JUST EARNED $%d FROM THE COMMUNITY CHEST!\n", curr_space->change_money);
+          money += curr_space->change_money;
+          new_update->gains[player_num] = money;
+        }
+        // draw a chance card
+        struct chance * curr_chance = getshm_chance();
+        printf("CHANCE CARD: YOU JUST DREW \"%s\".\n", curr_chance->text);
+        // update spot on board
+        new_turn->curr_index += curr_chance->spaces;
+        new_update->position[player_num] += curr_chance->spaces;
+        if (new_turn->curr_index > 39) {
+  	       money += 200;
+           new_update->gains[player_num] = money;
+  	       new_turn->curr_index = new_turn->curr_index % 40;
+           new_update->position[player_num] = new_turn->curr_index;
+        }
+        // paying each player $50 in special case
+        if (strcmp(curr_chance->text, "You have been elected Chairman of the Board - pay each player $50.") == 0) {
+          int all_players;
+          for (all_players = 0; all_players < 4; all_players++) {
+            if (all_players != player_num) {
+              new_update->gains[all_players] += 50;
+            }
+          }
+        }
+        // update money
+        if (money + curr_chance->money >= 0) {
+          money += curr_chance->money;
+          new_update->gains[player_num] = money;
+        } else {
+          printf("YOU ARE NOW BANKRUPT AND OUT OF THE GAME. GOODBYE.\n");
+          new_turn->dead = 1;
+        }
+        free(curr_chance);
+      }
+      // no chance card, land on space w/ someone else's property
+      else if (curr_space->property == 2) {
+        int money_owed = (curr_space->rent)[curr_space->houses_owned];
+        if (money - money_owed >= 0) {
+          //pay up (subtract money from your bank acct)
+          money -= money_owed;
+          new_update->gains[player_num] = money;
+          // add $$ to other person's bank acct
+          new_turn->dues[curr_space->owner] = money_owed;
+        }
+        else {
+          printf("YOU ARE NOW BANKRUPT AND OUT OF THE GAME. GOODBYE.\n");
+          new_turn->dead = 1;
+        }
+
+      }
+      // no chance card, land on space w/ property you can buy
+      else if (curr_space->property == 1) {
+          if (money - curr_space->cost < 0) {
+            printf("UNFORTUNATELY YOU DON'T HAVE ENOUGH MONEY TO PURCHASE THIS PROPERTY. TRY AGAIN LATER. \n");
+          }
+          else {
+            printf("WOULD YOU LIKE TO BUY THIS PROPERTY? THE INITIAL COST IS %d. YOU WILL EARN $%d EVERYTIME SOMEONE LANDS ON THIS SPACE. YOU CURRENTLY HAVE $%d. TYPE YES/NO.\n", curr_space->cost, curr_space->rent[0], money);
+            fgets(buffer, BUFFER_SIZE, stdin);
+            if (strcmp(buffer, "YES") == 0 || strcmp(buffer, "yes") == 0) {
+              //pay up (subtract money from your bank acct)
+              money -= curr_space->cost;
+              new_update->gains[player_num] = money;
+              // set new owner
+              curr_space->owner = player_num;
+              //buying houses
+              printf("WOULD YOU LIKE TO BUY ANY HOUSES? YOU CAN BUY UP TO FOUR HOUSES, WITH EACH HOUSE COSTING $%d. USERS WILL PAY YOU AN ADDITIONAL RENT OF $%d for 1 HOUSE, $%d for 2 HOUSES, $%d for 3 HOUSES, $%d for 4 HOUSES, INCLUDING THE ORIGINAL RENT OF %d. TYPE IN THE NUMBER OF HOUSES YOU WOULD LIKE TO BUY. ZERO IS AN ACCEPTABLE ANSWER. CHOOSE WISELY BECAUSE YOU CAN'T GO BACK. (dun dun dun) \n", curr_space->house_cost, curr_space->rent[1], curr_space->rent[2], curr_space->rent[3], curr_space->rent[4], curr_space->rent[0]);
+              fgets(buffer, BUFFER_SIZE, stdin);
+              //pay up (subtract money from your bank acct)
+              if (money - (curr_space->house_cost) * atoi(buffer) < 0) {
+                printf("UNFORTUNATELY YOU DID NOT CHOOSELY WISELY AND WILL NOT BE ABLE TO BUY ANY HOUSES ON THIS PROPERTY FOR THIS TURN. \n");
+              }
+              else {
+                money -= curr_space->house_cost * atoi(buffer);
+                new_update->gains[player_num] = money;
+                printf("YOU NOW OWN %s HOUSES ON THIS PROPERTY.\n", buffer);
+              }
+            }
+          }
+      }
+      // land on some weird space, income tax
+      else {
+        // income tax or luxury tax
+        if ((strcmp(curr_space->name, "Income tax") == 0) || (strcmp(curr_space->name, "Luxury Tax") == 0)) {
+          printf("PAY TAX OF %d.\n", curr_space->change_money);
+          if (money + curr_space->change_money < 0) {
+            printf("YOU ARE NOW BANKRUPT. GOODBYE.\n");
+            new_turn->dead = 1;
+          }
+          else {
+            money += curr_space->change_money;
+            new_update->gains[player_num] = money;
+          }
+        }
+
+        // electric company or water works
+        if ((strcmp(curr_space->name, "Water Works") == 0) || (strcmp(curr_space->name, "Electric Company") == 0)) {
+
+          // check to see if anyone owes
+          if (curr_space->owner != -1) {
+            printf("THIS SPACE IS OWNED BY PLAYER %d. PAY RENT OF %d.\n", curr_space->owner, curr_space->rent[0]);
+            if (money - curr_space->rent[0] < 0) {
+              printf("YOU ARE NOW BANKRUPT. GOODBYE.\n");
+              new_turn->dead = 1;
+            }
+            else {
+              // change money in player bank acct
+              money -= curr_space->rent[0];
+              new_update->gains[player_num] = money;
+              // add money to owner account
+              new_turn->dues[curr_space->owner] = curr_space->rent[0];
+            }
+          }
+
+          // ask player if they want to buy
+          else {
+            if (money - curr_space->cost < 0) {
+              printf("UNFORTUNATELY YOU DO NOT HAVE ENOUGH TO BUY THIS PROPERTY RIGHT NOW. TRY AGAIN LATER.\n");
+            }
+            else {
+              printf("WOULD YOU LIKE TO BUY THIS PROPERTY? THE INITIAL COST IS %d. YOU WILL EARN $%d EVERYTIME SOMEONE LANDS ON THIS SPACE. TYPE YES/NO.\n", curr_space->cost, curr_space->rent[0]);
+              fgets(buffer, BUFFER_SIZE, stdin);
+              if (strcmp(buffer, "YES") == 0 || strcmp(buffer, "yes") == 0) {
+                //pay up (subtract money from your bank acct)
+                money -= curr_space->cost;
+                new_update->gains[player_num] = money;
+                // set new owner
+                curr_space->owner = player_num;
+              }
+            }
+          }
+        }
+        // go to jail
+        if (strcmp(curr_space->name, "Go to Jail") == 0) {
+          printf("PAY JAIL FINE OF $500.\n");
+          if (money + curr_space->change_money < 0) {
+            printf("YOU ARE NOW BANKRUPT. GOODBYE.");
+            new_turn->dead = 1;
+          }
+          else {
+            // change money in player bank acct
+            money += curr_space->change_money;
+            new_update->gains[player_num] = money;
+          }
+        }
+      }
+
+      // if bankrupt free then exit
+      // free all
+      free(new_turn);
+      free(new_update);
+      free(curr_space);
     } else {
       printf("\nIt is currently player[%d]'s turn\n", new_update->curr_player);
     }
-  
+
   }
 
 }
